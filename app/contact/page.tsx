88859d167dcb1d71e14f1bb7ee18f7dba2156b25
DenZaiyy <denzaiyy@outlook.fr>
2025-02-10 17:07:09 +0100

    feat: init contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold mb-6">Contact</h1>
+        </div>
+    );
+};
+
+export default ContactPage;
