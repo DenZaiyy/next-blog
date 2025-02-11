2b5edb42e0cdf97c9b3591fbef0826fd7674d669
DenZaiyy <denzaiyy@outlook.fr>
2025-02-11 11:09:34 +0100

    fix: baseURL with env variable

--- app/api/article/hooks/fetchArticle.ts
+++ app/api/article/hooks/fetchArticle.ts
@@ -1,11 +1,8 @@
 import { redirect } from "next/navigation";
 
 export const fetchArticle = async (articleId: string): Promise<TArticleWithTagsAndComments> => {
-    const baseUrl = process.env.NODE_ENV === "production"
-    ? "https://next-blog-38tkaycy3-denzaiyys-projects.vercel.app/"
-    : "http://localhost:3000";
 
-    const res = await fetch(`${baseUrl}/api/article/${articleId}`, {
+    const res = await fetch(`${process.env.BASE_URL}/api/article/${articleId}`, {
         cache: "no-store"
     });
 
--- app/api/article/hooks/fetchArticles.ts
+++ app/api/article/hooks/fetchArticles.ts
@@ -1,11 +1,8 @@
 import { redirect } from "next/navigation";
 
 export const fetchArticles = async (): Promise<TArticleWithTagsAndComments[]> => {
-    const baseUrl = process.env.NODE_ENV === "production"
-        ? "https://next-blog-38tkaycy3-denzaiyys-projects.vercel.app/"
-        : "http://localhost:3000";
 
-    const res = await fetch(`${baseUrl}/api/article/`, {
+    const res = await fetch(`${process.env.BASE_URL}/api/article/`, {
         cache: "no-store"
     });
 
